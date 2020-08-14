32b9c663b20bcc9245ef78ef0d2ca7f2022a7819
max-Z7 <zaraz121801@gmail.com>
2020-08-15 00:34:38 +0300

    Обновление меню

--- sweetsleep.py
+++ sweetsleep.py
@@ -1,63 +1,23 @@
 # А еще это написано на гребанном, блять, vim #
 
-from os import system, getppid
-from os.path import basename, abspath
-from time import sleep
-import termux, sys
+import sys
+from smod import volis, getper, cdown
 
-def volis(x):    # volume is
-    vol = eval(termux.volume())
-    music = vol[3].get(x)
-    return music 
+cv = lambda a: (a-1)**2   # Change Value
 wifi = 1
-mvol = volis("volume")  # max volume
-getper = lambda a: 60 * a / mvol    # get period
-def timein():
+sdown = 1
+vol = volis()
+def timein():   # time input
     while True:
-        try:
+#        try:
             m = float(input('Время, завершения работы (мин): '))
-            per = getper(m)
+            per = getper(m, vol)
             break
-        except:
-            print('Прости, ты ошибся. П-пожалуйста, напиши число...')
+#        except:
+#            print('Прости, ты ошибся. П-пожалуйста, напиши число...')
     return per
-def cdown(per):
-    print('Запускаю обратный отчет.')
-    for i in range(mvol, 0, -1):
-        sleep(per)
-        com = 'termux-volume music ' + str(i) 
-        system(com)
-        print("Volume: " + str(i))
-    # ending
-    bye = 'Сладких снов, хозяин.'
-    system('termux-wifi-enable false' * wifi)
-    print(bye)
-    system('termux-toast "{}"'.format(bye))
-    system("kill -9 %d"%(getppid()))
 
-argv = sys.argv
-if len(argv) >= 2:
-    if argv[1] == '-d':
-        print('Использую настройки по умолчанию')
-        cdown(getper(30))
-    elif  argv[1] == '-h':
-        print(argv[0] + \
-''' [Option]
-
--h  Выводит это сообщение
--d  Запустить с настройками по умолчанию (30 мин)
-''')
-    elif argv[1] == '-s':
-        script = open('/data/data/com.termux/files/usr/bin/sweetsleep', 'w')
-        script.write('#! /bin/bash\npython3 {}'.format(abspath(__file__)))
-        script.close()
-        system('chmod +x /data/data/com.termux/files/usr/bin/./sweetsleep')
-        abspath(__file__)
-    else:
-        print('Недопустимая опция: попробуй -h, п-пожалуйста.')
-else:
-
-    print(\
+print(\
 '''
 #--##-##      *
      #x#             _--_
@@ -78,11 +38,36 @@ else:
             SW╒╒T    .
             SL└└P
 ''')
-    while True:
-        com = input('> ')
-        if com == 'exit':
-            break
-        elif com == 'start':
-            cdown(timein())
+while True:
+    com = input('> ')
+    if com == 'exit':
+        quit()
+    elif com == 'help':
+        print('''\
+help     выводит это сообщение
+start    запускает таймер сна
+wifi     вкл/выкл Wi-Fi после таймера
+shutdown выкл/вкл отключение сессии
+debug    ???
+exit     выход из программы''')
+    elif com == 'start':
+        break
+    elif com == 'wifi':
+        wifi = cv(wifi)
+        if wifi == 0:
+            print('Я не буду отключать Wi-Fi')
         else:
-            print('Я не знаю такой команды(')
+            print('Ладно, я его отключу...')
+    elif com == 'shutdown':
+        sdown = cv(sdown)
+        if sdown == 0:
+            print('Сессия продолжит работу')
+        else:
+            print('Хорошо, я закрою за тебя сессию')
+    elif com == 'debug':
+        print('music:', volis())
+        print('wifi:', wifi)
+        print('sdown:', sdown)
+    else:
+        print('Я не знаю такой команды(\nПопробуй help')
+cdown(timein(), wifi, sdown)
